edaca96982e331d4307d00d83806059c8ce56d0f
Christian Guevara <chrisdb150@gmail.com>
2023-04-03 20:30:36 -0400

    homewrok

--- src/components/ui/UiTemplate.tsx
+++ src/components/ui/UiTemplate.tsx
@@ -2,14 +2,14 @@ import React, { Children, FC, PropsWithChildren } from 'react'
 
 export const UiTemplate: FC<PropsWithChildren> = ({ children }) => {
     return (
-        /* Bar nav */
-        <main>
-            <div style={ { background: '#000', padding: '16px', display: 'flex' } }>
-                <p>login</p>
-            </div>
-            { children }
-        </main>
 
-        /* Footer */
+        <>
+
+            <nav></nav>
+            <main>
+                { children }
+            </main>
+            <footer></footer>
+        </>
     )
 }
